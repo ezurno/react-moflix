f665a4903041a5272ba1082b12ca685b723bc2ce
ezurno <ezurno@gmail.com>
2023-02-20 20:13:48 +0900

    SET: set background-post
    1. util 으로 url을 관리하는 함수 생성
    
    2. background-image를 linear-gradient를 줌

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,11 +1,87 @@
 import { useQuery } from "react-query";
+import styled from "styled-components";
 import { getMovies } from "../api";
+import { makeImagePath } from "../Utils/utilities";
+
+const Wrapper = styled.div`
+  background-color: black;
+  height: 200vh;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding-left: 60px;
+
+  background-image: linear-gradient(
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0),
+      rgba(0, 0, 0, 1)
+    ),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 48px;
+  margin-bottom: 12px;
+`;
+
+const Overview = styled.p`
+  font-size: 24px;
+  width: 40vw;
+`;
+
+export interface IMoviesData {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
 
 function Home() {
-  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
+  const { data, isLoading } = useQuery<IMoviesData>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
   console.log(data, isLoading);
 
-  return <div style={{ height: "200vh" }}>home</div>;
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].poster_path || "")}>
+            <Title>{data?.results[0].title.toUpperCase()}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
